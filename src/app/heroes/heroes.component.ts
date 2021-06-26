9ec3ee10eb820a79f6bb1920460c3f11e50bdb00
Marcelo Ferrer <marceloferrer@live.com.ar>
2021-06-26 12:42:35 -0300

    Add display list to heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  hero : Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes : Hero[] = HEROES
+
+  ngOnInit(): void {
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
